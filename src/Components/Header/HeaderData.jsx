943b0ade903981546b0b9c3f4cc5971df8c76270
Ubaid Ullah <khanxx777@gmail.com>
2023-07-25 10:50:42 +0500

    ubaid

---
+++ src/Components/Header/HeaderData.jsx
@@ -0,0 +1,24 @@
+const HeaderData = [
+    {
+        title: 'Home',
+        link: './'
+    },
+    {
+        title: 'About Us',
+        link:'./page/about-us'
+       
+    },
+    {
+        title: 'Menu',
+        link:'./page/about-us'
+       
+    },
+    {
+        title: 'Contact Us',
+        link:'./page/about-us'
+       
+    },
+  
+]
+
+export default HeaderData
